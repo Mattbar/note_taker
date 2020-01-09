000e2813ddb1571b017ca0aa4c468130c5301cb6
Mattbar <mlbarnes04@gmail.com>
2020-01-09 14:46:58 -0800

    update home screen

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,8 +10,7 @@ class Home extends Component {
     const { isLoggingOut, logoutError } = this.props;
     return (
       <div>
-        <h1>This is your app's protected area.</h1>
-        <p>Any routes here will also be protected</p>
+        <h1> TAKE NOTES</h1>
         <button onClick={this.handleLogout}>Logout</button>
         {isLoggingOut && <p>Logging Out....</p>}
         {logoutError && <p>Error logging out</p>}
